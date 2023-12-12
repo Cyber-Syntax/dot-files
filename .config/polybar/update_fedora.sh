53c9bbe1bde6b7590804955c22a74d026052c2be
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2023-12-12 18:17:01 +0300

    Update Git repository with file mode changes and new files

--- .config/polybar/update_fedora.sh
+++ .config/polybar/update_fedora.sh
@@ -2,14 +2,10 @@
 
 printf "\033[1m\033[7mUpdating Fedora\033[0m"
 printf "\n"
-
-# Update Fedora 
 sudo dnf update -y --refresh --allowerasing
 
 printf "\033[1m\033[7mUpdating Flatpak\033[0m"
 printf "\n"
-
-# Update Flatpak
 flatpak update -y
 
 printf "\033[1m\033[7mUpdate completed\033[0m"
--- .config/polybar/updates-dnf.py
+++ .config/polybar/updates-dnf.py
@@ -2,30 +2,61 @@
 """Check for updates using dnf"""
 import subprocess
 
-try:
+def dnf_update():
+    """Check for updates using dnf"""
+    # find how much update available for dnf
     update = subprocess.check_output(["dnf", "updateinfo","-q", "--list"], stderr=subprocess.STDOUT)
-    # find how much update available for flatpak
-    update_flatpak = subprocess.check_output(["flatpak", "remote-ls", "--updates", "--user", "flathub"]).decode("utf-8")
 
+    # count
+    update_count = int(update.decode("utf-8").count("\n"))
+
+    return update_count
+
+def flatpak_update():
+    """Check for updates using flatpak"""
+    # find how much update available for flatpak
+    update_flatpak = subprocess.check_output(["flatpak", "remote-ls",
+                                                "--updates", "--user", "flathub"]).decode("utf-8")
     # split the output into lines
     lines = update_flatpak.split("\n")
 
     # count the number of lines
     update_count_flatpak = len(lines) - 1 # Subtract 1 because the last line is empty
 
-    # print the number of updates
-    if update_count_flatpak > 0:
-        print(f"| Flatpak: {update_count_flatpak}", end=" | ")
-    else:
-        print(f"| Flatpak: Up to date", end=" | ")
+    return update_count_flatpak
 
-    # count the number of updates
-    update_count = int(update.decode("utf-8").count("\n"))
+def print_update(update_count, update_count_flatpak):
+    """Print the update count"""
+    # Check if both are up to date
+    if update_count == 0 and update_count_flatpak == 0:
+        print("System is up to date")
 
+    # dnf
     if update_count > 0:
-        print("DNF:", str(update_count) + " |")
-    elif update_count == 0:
-        print("DNF:", "Up to date |")
+        print(f"Updates available: {update_count}")
+    else:
+        print("DNF: Up to date")
 
-except subprocess.CalledProcessError:
-    print("| No Internet Connection |")
+    # flatpak
+    if update_count_flatpak > 0:
+        print(f"Flatpak Updates available: {update_count_flatpak}")
+    else:
+        print("Flatpak: Up to date")
+
+def main():
+    try:
+        # find how much update available for dnf
+        dnf_count = dnf_update()
+
+        # find how much update available for flatpak
+        flatpak_count = flatpak_update()
+
+        # print the update count
+        print_update(dnf_count, flatpak_count)
+    except subprocess.CalledProcessError:
+        print("Error: dnf or flatpak not installed")
+    except Exception as exception:
+        print("Error:", exception)
+
+if __name__ == "__main__":
+    main()
